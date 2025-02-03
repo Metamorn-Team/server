792dcc94b6fd9db67aba35debea819ed1c5fcd9a
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-02-03 15:37:55 +0900

    feat: 구글 strategy 구현

---
+++ src/common/constants.ts
@@ -0,0 +1,2 @@
+export const GOOGLE_USER_INFO_URL =
+    'https://www.googleapis.com/oauth2/v3/userinfo';
---
+++ src/infrastructure/auth/strategy/google.strategy.ts
@@ -0,0 +1,35 @@
+import { GOOGLE_USER_INFO_URL } from 'src/common/constants';
+import { OauthStrategy, OauthUserInfo } from './auth.strategy';
+import { InternalServerErrorException } from '@nestjs/common';
+
+export class GoogleStrategy implements OauthStrategy {
+    async getUserInfo(token: string): Promise<OauthUserInfo> {
+        const response = await fetch(GOOGLE_USER_INFO_URL, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        if (!response.ok) {
+            throw new InternalServerErrorException(
+                `Google API 에러: ${response.status} ${response.statusText}`,
+            );
+        }
+
+        const body: GoogleUserInfo = await response.json();
+        const { name, email } = body;
+
+        return {
+            name,
+            email,
+            provider: 'GOOGLE',
+        };
+    }
+}
+
+interface GoogleUserInfo {
+    sub: string;
+    name: string;
+    email: string;
+    email_verified: boolean;
+}
