bd3fdda7e41a385c13d6d94bd5e1fef8d9e05db5
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-02-03 15:56:29 +0900

    feat: auth context 구현현

---
+++ src/infrastructure/auth/context/auth-context.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Provider } from 'src/shared/types';
+import { OauthUserInfo } from '../strategy/oauth.strategy';
+import { GoogleStrategy } from '../strategy/google.strategy';
+
+@Injectable()
+export class OauthContext {
+    constructor(private readonly googleStrategy: GoogleStrategy) {}
+
+    async getUserInfo(
+        provider: Provider,
+        token: string,
+    ): Promise<OauthUserInfo> {
+        // 카카오 네이버 구현 완료 후 추가 필요
+        return provider === 'GOOGLE'
+            ? await this.googleStrategy.getUserInfo(token)
+            : provider === 'KAKAO'
+            ? { email: 'kakao', name: 'john doe', provider: 'KAKAO' }
+            : { email: 'naver', name: 'john doe', provider: 'NAVER' };
+    }
+}
