9583fc0def410dd4739c6547ae26e0c22d24128b
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-02-08 15:42:28 +0900

    chore: 모듈 의존성 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from 'src/app.controller';
 import { AppService } from 'src/app.service';
+import { UserModule } from 'src/modules/user/user.module';
+import { AuthModule } from 'src/modules/auth/auth.module';
+import { PrismaModule } from './infrastructure/prisma/prisma.module';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        PrismaModule,
+        UserModule,
+        AuthModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
