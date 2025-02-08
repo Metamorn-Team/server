349aa47ef1ea8b552049aea6ce2f0b2e4f829513
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-02-08 12:41:25 +0900

    chore: 스키마 변경에 따른 타입 수정

--- src/domain/types/uesr.types.ts
+++ src/domain/types/uesr.types.ts
@@ -3,8 +3,12 @@ import { Provider } from 'src/shared/types';
 export interface UserInfo {
     readonly id: string;
     readonly email: string;
-    readonly accountId: string;
     readonly nickname: string;
     readonly tag: string;
     readonly provider: Provider;
+}
+
+export interface UserPrototype {
+    readonly id: string;
+    readonly email: string;
 }
