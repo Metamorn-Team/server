21cce0b4e21510e36001e927089c586a4ff7674e
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-02-10 14:14:41 +0900

    feat: refresh 토큰 구현

--- src/presentation/auth/auth.controller.ts
+++ src/presentation/auth/auth.controller.ts
@@ -1,26 +1,67 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from 'src/domain/services/auth/auth.service';
 import { Provider } from 'src/shared/types';
 import { LoginRequest } from 'src/presentation/dto/auth/request/login.request';
 import { LoginResponse } from 'src/presentation/dto/auth/response/login.response';
 import { RegisterRequest } from 'src/presentation/dto/auth/request/register.request';
 import { RegisterResponse } from 'src/presentation/dto/auth/response/register.response';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    private readonly refreshCookieTime: number;
+
+    constructor(
+        private readonly authService: AuthService,
+        private readonly configService: ConfigService,
+    ) {
+        this.refreshCookieTime = this.configService.get<number>(
+            'REFRESH_COOKIE_TIME',
+        ) as number;
+    }
 
     @Post(':provider/login')
     async login(
         @Param('provider') provider: Provider,
         @Body() dto: LoginRequest,
+        @Res({ passthrough: true }) response: Response,
     ): Promise<LoginResponse> {
         const { accessToken } = dto;
-        return await this.authService.login(provider, accessToken);
+
+        const loginResponse = await this.authService.login(
+            provider,
+            accessToken,
+        );
+
+        const { refreshToken, ...responseWithoutRefresh } = loginResponse;
+
+        response.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: this.refreshCookieTime,
+        });
+
+        return responseWithoutRefresh;
     }
 
     @Post('register')
-    async register(@Body() dto: RegisterRequest): Promise<RegisterResponse> {
-        return this.authService.register(dto);
+    async register(
+        @Body() dto: RegisterRequest,
+        @Res({ passthrough: true }) response: Response,
+    ): Promise<RegisterResponse> {
+        const registerResponse = await this.authService.register(dto);
+
+        const { refreshToken, ...responseWithoutRefresh } = registerResponse;
+
+        response.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: this.refreshCookieTime,
+        });
+
+        return responseWithoutRefresh;
     }
 }
