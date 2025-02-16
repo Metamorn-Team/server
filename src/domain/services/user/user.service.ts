4c622ae8f529eb9eee6bff3079afc8e89d7db93f
magik99 <hojun0810@naver.com>
2025-02-16 19:34:36 +0900

    feat: prisma 예외 처리

--- src/domain/services/user/user.service.ts
+++ src/domain/services/user/user.service.ts
@@ -1,4 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { ConflictException } from 'src/domain/exceptions/exceptions';
 import { UserRepository } from 'src/domain/interface/user.repository';
 
 @Injectable()
@@ -13,6 +15,15 @@ export class UserService {
     }
 
     async updateTag(userId: string, tag: string) {
-        await this.userRepository.update({ id: userId, tag });
+        try {
+            await this.userRepository.update({ id: userId, tag });
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ConflictException('이미 사용 중인 태그입니다.');
+                }
+            }
+            throw error;
+        }
     }
 }
