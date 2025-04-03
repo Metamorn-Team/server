3aafd8299621fc0cc507e9907c646b99551773c7
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-04-03 18:17:58 +0900

    chore: validatePipe 추가

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
@@ -6,6 +7,13 @@ import { AppModule } from 'src/app.module';
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.use(cookieParser());
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+        }),
+    );
     await app.listen(3000);
 }
 bootstrap();
