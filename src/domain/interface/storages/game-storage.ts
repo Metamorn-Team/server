5d9f2e74ccc3dcf6752f8a699860da8a88f13516
Haesung <koh1260@naver.com>
2025-04-06 05:32:33 +0900

    feat: 웹소켓 플레이어 및 방 관리 스토리지 정의
    - 스토리지 추상화에 의존하도록 interface 구현
    - 메모리 스토리지 구현체 구현

---
+++ src/domain/interface/storages/game-storage.ts
@@ -0,0 +1,19 @@
+import { Player, Room, RoomType } from 'src/domain/types/game.types';
+
+export interface GameStorage {
+    getPlayer(clientId: string): Player | null;
+    addPlayer(clientId: string, player: Player): void;
+    deletePlayer(clientId: string): void;
+
+    createRoom(roomId: string, room: Room): void;
+    getRoom(roomId: string): Room | null;
+    getRoomOfType(type: RoomType): Set<string> | null;
+    getRoomIdsByType(type: RoomType): string[];
+    addRoomOfType(type: RoomType, roomId: string): void;
+
+    getPlayerStore(): Record<string, Player>;
+    getRoomStore(): Record<string, Room>;
+    getRoomOfTypeStore(): Record<RoomType, Set<string>>;
+}
+
+export const GameStorage = Symbol('GameStorage');
