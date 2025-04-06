c19fcc92f21b702289caaa9f2f4e2668f90faf2d
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-04-06 17:13:29 +0900

    chore: swagger setup 구현

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from 'src/app.module';
+import { setupSwagger } from 'src/configs/swagger/setup-swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.use(cookieParser());
+    setupSwagger(app);
     await app.listen(3000);
 }
 bootstrap();
