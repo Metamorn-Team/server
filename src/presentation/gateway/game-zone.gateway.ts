cf322c484f3c1dfebf8585f565d89883b979ee14
Haesung <koh1260@naver.com>
2025-04-09 21:06:38 +0900

    fix: 메세지 전송 이벤트에서 플레이어 식별자 수정

--- src/presentation/gateway/game-zone.gateway.ts
+++ src/presentation/gateway/game-zone.gateway.ts
@@ -1,5 +1,5 @@
 import { Logger, UseGuards } from '@nestjs/common';
-import { Namespace, Socket } from 'socket.io';
+import { Namespace } from 'socket.io';
 import {
     ConnectedSocket,
     MessageBody,
@@ -10,6 +10,7 @@ import {
     WebSocketGateway,
     WebSocketServer,
 } from '@nestjs/websockets';
+import { v4 } from 'uuid';
 import { CurrentUserFromSocket } from 'src/common/decorator/current-user.decorator';
 import { WsAuthGuard } from 'src/common/guard/ws-auth.guard';
 import { ZoneService } from 'src/domain/services/game/zone.service';
@@ -17,7 +18,6 @@ import { UserReader } from 'src/domain/components/users/user-redear';
 import { PlayerJoinRequest } from 'src/presentation/dto/game/request/player-join.request';
 import { TypedSocket } from 'src/presentation/dto/game/socket/type';
 import { SendMessageRequest } from 'src/presentation/dto/game/request/send-message.request';
-import { v4 } from 'uuid';
 
 @UseGuards(WsAuthGuard)
 @WebSocketGateway({
@@ -124,8 +124,11 @@ export class GameZoneGateway
         @ConnectedSocket() client: TypedSocket,
         @CurrentUserFromSocket() senderId: string,
     ) {
+        this.logger.debug(`전송자: ${senderId}`);
+        this.logger.debug(`메시지: ${data.message}`);
+
         // 채팅 저장 비동기
-        const sender = this.zoneService.getPlayer(senderId);
+        const sender = this.zoneService.getPlayer(client.id);
 
         if (!sender) return;
 
