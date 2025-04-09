88612866552a1896a55bf0c718d1526b7ba06904
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-04-10 02:03:57 +0900

    test: 친구 요청 예외 케이스 작성

--- test/e2e/friend.e2e-spec.ts
+++ test/e2e/friend.e2e-spec.ts
@@ -1,12 +1,13 @@
 import * as request from 'supertest';
 
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
 import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
 import { generateUserEntity } from 'test/helper/generators';
 import { login } from 'test/helper/login';
 import { SendFriendRequest } from 'src/presentation/dto/friends/request/send-friend.request';
+import { v4 } from 'uuid';
 
 describe('FriendController (e2e)', () => {
     let app: INestApplication;
@@ -61,6 +62,63 @@ describe('FriendController (e2e)', () => {
 
             expect(status).toBe(204);
             expect(createdFriendRequest).not.toBeNull();
+        });
+
+        it('이미 보낸 요청이 있을 경우 409 Conflict 에러 반환', async () => {
+            const { accessToken, userId } = await login(app);
+
+            const receivedUser = await prisma.user.create({
+                data: generateUserEntity(
+                    'receiver_conflict@email.com',
+                    'conflictNickname',
+                    'conflictTag',
+                ),
+            });
+
+            const dto: SendFriendRequest = {
+                targetUserId: receivedUser.id,
+            };
+
+            await prisma.friendRequest.create({
+                data: {
+                    id: v4(),
+                    senderId: userId,
+                    receiverId: receivedUser.id,
+                    status: 'PENDING',
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+            });
+
+            const response = await request(app.getHttpServer())
+                .post('/friends/requests')
+                .send(dto)
+                .set('Authorization', accessToken);
+
+            expect(response.status).toBe(HttpStatus.CONFLICT);
+
+            const requestCount = await prisma.friendRequest.count({
+                where: {
+                    senderId: userId,
+                    receiverId: receivedUser.id,
+                },
+            });
+            expect(requestCount).toBe(1);
+        });
+
+        it('자신에게 친구 요청을 보낼 경우 400 Bad Request 에러 반환', async () => {
+            const { accessToken, userId } = await login(app);
+
+            const dto: SendFriendRequest = {
+                targetUserId: userId,
+            };
+
+            const response = await request(app.getHttpServer())
+                .post('/friends/requests')
+                .send(dto)
+                .set('Authorization', accessToken);
+
+            expect(response.status).toBe(HttpStatus.BAD_REQUEST); // 400 확인
         });
     });
 });
