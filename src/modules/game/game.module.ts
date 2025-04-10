7a56a40cafbbc63eb16a220d9c0f8475e937b17f
Haesung <koh1260@naver.com>
2025-04-10 21:55:46 +0900

    chore: 누락된 모듈

--- src/modules/game/game.module.ts
+++ src/modules/game/game.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { GameStorage } from 'src/domain/interface/storages/game-storage';
 import { ZoneService } from 'src/domain/services/game/zone.service';
 import { MemoryStorage } from 'src/infrastructure/storages/memory-storage';
+import { ChatMessageModule } from 'src/modules/chat-messages/chat-message.module';
 import { IslandJoinComponentModule } from 'src/modules/island-joins/island-join-component.module';
 import { IslandComponentModule } from 'src/modules/islands/island-component.module';
 import { UserComponentModule } from 'src/modules/users/users-component.module';
@@ -12,6 +13,7 @@ import { GameZoneGateway } from 'src/presentation/gateway/game-zone.gateway';
         UserComponentModule,
         IslandComponentModule,
         IslandJoinComponentModule,
+        ChatMessageModule,
     ],
     providers: [
         GameZoneGateway,
