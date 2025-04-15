f692fe384df7fe675817fa03f3d6259e9666fa72
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-04-15 11:11:23 +0900

    chore: api 작성 및 삼항연산자 리팩토링

--- src/modules/friends/friends-component.module.ts
+++ src/modules/friends/friends-component.module.ts
@@ -4,12 +4,14 @@ import { FriendWriter } from 'src/domain/components/friends/friend-writer';
 import { FriendChecker } from 'src/domain/components/friends/friend-checker';
 import { FriendRepository } from 'src/domain/interface/friend.repository';
 import { FriendPrismaRepository } from 'src/infrastructure/repositories/friend-prisma.repository';
+import { UserReader } from 'src/domain/components/users/user-reader';
 
 @Module({
     providers: [
         FriendReader,
         FriendWriter,
         FriendChecker,
+        UserReader,
         { provide: FriendRepository, useClass: FriendPrismaRepository },
     ],
     exports: [FriendReader, FriendWriter, FriendChecker],
