94ed728fd3af0301ad0df60d7e547b5ca3c01ce3
magik99 <hojun0810@naver.com>
2025-04-17 17:15:06 +0900

    chore: 미사용 FriendReader 삭제 및 의존성 제거

--- src/domain/components/friends/friend-writer.ts
+++ src/domain/components/friends/friend-writer.ts
@@ -1,7 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { FriendEntity } from 'src/domain/entities/friend/friend.entity';
 import { FriendRepository } from 'src/domain/interface/friend.repository';
-import { FriendReader } from './friend-reader';
 import { FriendStatus } from 'src/domain/types/friend.types';
 
 @Injectable()
@@ -9,7 +8,6 @@ export class FriendWriter {
     constructor(
         @Inject(FriendRepository)
         private readonly friendsRepository: FriendRepository,
-        private readonly friendReader: FriendReader,
     ) {}
 
     async create(friend: FriendEntity): Promise<void> {
