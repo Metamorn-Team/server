926d3fc97db82c3082e8565b2a413696b47686d4
Haesung <koh1260@naver.com>
2025-04-19 04:10:10 +0900

    feat: 친구 목록에 접속 여부 속성 추가

--- src/modules/friends/friends.module.ts
+++ src/modules/friends/friends.module.ts
@@ -3,9 +3,10 @@ import { FriendsComponentModule } from './friends-component.module';
 import { FriendsController } from 'src/presentation/controller/friends/friends.controller';
 import { FriendsService } from 'src/domain/services/friends/friends.service';
 import { UserComponentModule } from '../users/users-component.module';
+import { GameStorageModule } from 'src/modules/game/game-storage.module';
 
 @Module({
-    imports: [FriendsComponentModule, UserComponentModule],
+    imports: [FriendsComponentModule, UserComponentModule, GameStorageModule],
     controllers: [FriendsController],
     providers: [FriendsService],
 })
