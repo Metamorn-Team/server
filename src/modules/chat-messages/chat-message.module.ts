e924e34ed0fc44b1369a6366b39c4f1540ba3d64
Haesung <koh1260@naver.com>
2025-04-18 22:54:52 +0900

    refactor: 게임 스토리지 모듈 분리

--- src/modules/chat-messages/chat-message.module.ts
+++ src/modules/chat-messages/chat-message.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatMessageService } from 'src/domain/services/chat-messages/chat-message.service';
 import { ChatMessageComponentModule } from 'src/modules/chat-messages/chat-message-component.module';
+import { GameStorageModule } from 'src/modules/game/game-storage.module';
 
 @Module({
-    imports: [ChatMessageComponentModule],
+    imports: [GameStorageModule, ChatMessageComponentModule],
     providers: [ChatMessageService],
     exports: [ChatMessageService],
 })
--- src/modules/game/game.module.ts
+++ src/modules/game/game.module.ts
@@ -3,6 +3,7 @@ import { GameStorage } from 'src/domain/interface/storages/game-storage';
 import { ZoneService } from 'src/domain/services/game/zone.service';
 import { MemoryStorage } from 'src/infrastructure/storages/memory-storage';
 import { ChatMessageModule } from 'src/modules/chat-messages/chat-message.module';
+import { GameStorageModule } from 'src/modules/game/game-storage.module';
 import { IslandJoinComponentModule } from 'src/modules/island-joins/island-join-component.module';
 import { IslandComponentModule } from 'src/modules/islands/island-component.module';
 import { UserComponentModule } from 'src/modules/users/users-component.module';
@@ -10,15 +11,12 @@ import { GameZoneGateway } from 'src/presentation/gateway/game-zone.gateway';
 
 @Module({
     imports: [
+        GameStorageModule,
         UserComponentModule,
         IslandComponentModule,
         IslandJoinComponentModule,
         ChatMessageModule,
     ],
-    providers: [
-        GameZoneGateway,
-        ZoneService,
-        { provide: GameStorage, useClass: MemoryStorage },
-    ],
+    providers: [GameZoneGateway, ZoneService],
 })
 export class GameModule {}
