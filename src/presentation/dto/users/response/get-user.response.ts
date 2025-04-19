27d84704326fa2c28bfbf90c04af9f2154bdb47c
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2025-04-20 06:43:23 +0900

    chore: 응답 dto  옵셔널 제거

--- src/presentation/dto/users/response/get-user.response.ts
+++ src/presentation/dto/users/response/get-user.response.ts
@@ -1,35 +1,38 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import { Provider } from '../../shared';
 
 export class GetUserResponse {
     @ApiProperty({
-        description: '사용자 ID(UUID V4)',
+        description: '타겟 사용자 ID(UUID V4)',
         example: '123e4567-e89b-12d3-a456-426614174000',
     })
     readonly id: string;
 
-    @ApiProperty({ description: '사용자 이메일', example: 'user@eamil.com' })
+    @ApiProperty({
+        description: '타겟 사용자의 이메일',
+        example: 'user@eamil.com',
+    })
     readonly email: string;
 
-    @ApiProperty({ description: '사용자 닉네임', example: 'nickname' })
+    @ApiProperty({ description: '타겟 사용자의 닉네임', example: 'nickname' })
     readonly nickname: string;
 
-    @ApiProperty({ description: '사용자 태그', example: 'tag' })
+    @ApiProperty({ description: '타겟 사용자의 태그', example: 'tag' })
     readonly tag: string;
 
     @ApiProperty({
-        description: '소셜 로그인 제공자',
+        description: '타겟 사용자의 소셜 로그인 제공자',
         enum: ['GOOGLE', 'KAKAO', 'NAVER'],
     })
     readonly provider: Provider;
 
-    @ApiProperty({ description: '사용자 아바타 키' })
+    @ApiProperty({ description: '타겟 사용자의 아바타 키' })
     readonly avatarKey: string;
 
-    @ApiPropertyOptional({
-        description: '요청자와 친구 상태일 경우 ACCEPTED 아닐 경우 null',
-        nullable: true,
+    @ApiProperty({
+        description:
+            '요청자와 타겟 유저가 친구 상태일 경우 ACCEPTED 아닐 경우 null',
         example: 'ACCEPTED',
     })
-    readonly friendStatus?: string | null;
+    readonly friendStatus: string | null;
 }
