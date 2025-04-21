63e64bd380c7b32c3b975ff59d010053d8bfcd29
Haesung <koh1260@naver.com>
2025-04-22 08:04:16 +0900

    feat: 친구 여부 확인 기능 구현

--- src/presentation/dto/shared.ts
+++ src/presentation/dto/shared.ts
@@ -1,3 +1,4 @@
 export type RoomType = 'dev' | 'design';
 export type Provider = 'GOOGLE' | 'KAKAO' | 'NAVER';
 export type FrinedStatus = 'ACCEPTED' | 'PENDING' | 'REJECTED';
+export type FriendRequestStatus = 'ACCEPTED' | 'SENT' | 'RECEIVED' | 'NONE';
