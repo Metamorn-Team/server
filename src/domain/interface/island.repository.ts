65b21271180b00c60bf9b35ccdc317d9fa0f8c12
Haesung <koh1260@naver.com>
2025-04-30 18:47:27 +0900

    feat: 섬 단건 조회 메서드 구현

--- src/domain/interface/island.repository.ts
+++ src/domain/interface/island.repository.ts
@@ -1,7 +1,9 @@
 import { IslandEntity } from 'src/domain/entities/islands/island.entity';
+import { Island } from 'src/domain/types/game.types';
 
 export interface IslandRepository {
     save(data: IslandEntity): Promise<void>;
+    findOneById(id: string): Promise<Island | null>;
 }
 
 export const IslandRepository = Symbol('IslandRepository');
