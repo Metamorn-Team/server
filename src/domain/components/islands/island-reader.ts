501bafcab193884b06cae6608c5e986996290f41
Haesung <koh1260@naver.com>
2025-05-07 07:01:31 +0900

    refactor: island reader에서 메모리 조회 관련 코드 제거

--- src/domain/components/islands/island-reader.ts
+++ src/domain/components/islands/island-reader.ts
@@ -3,7 +3,6 @@ import { DomainExceptionType } from 'src/domain/exceptions/enum/domain-exception
 import { DomainException } from 'src/domain/exceptions/exceptions';
 import { ISLAND_NOT_FOUND_MESSAGE } from 'src/domain/exceptions/message';
 import { IslandRepository } from 'src/domain/interface/island.repository';
-import { NormalIslandStorage } from 'src/domain/interface/storages/normal-island-storage';
 import { Island } from 'src/domain/types/game.types';
 
 @Injectable()
@@ -11,8 +10,6 @@ export class IslandReader {
     constructor(
         @Inject(IslandRepository)
         private readonly islandRepository: IslandRepository,
-        @Inject(NormalIslandStorage)
-        private readonly normalIslandStorage: NormalIslandStorage,
     ) {}
 
     async readOne(id: string): Promise<Island> {
@@ -26,24 +23,5 @@ export class IslandReader {
         }
 
         return island;
-    }
-
-    readLiveIsland(page: number, limit = 20) {
-        const islands = this.normalIslandStorage.getAllIsland();
-
-        islands.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
-
-        const start = (page - 1) * limit;
-        const end = start + limit;
-
-        return islands.slice(start, end).map((island) => ({
-            id: island.id,
-            maxMembers: island.max,
-            countParticipants: island.players.size,
-            name: island.name,
-            description: island.description,
-            coverImage: island.coverImage,
-            tags: island.tags,
-        }));
     }
 }
