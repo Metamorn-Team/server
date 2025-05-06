9b31ba7b56bbf44f185a5b0d5f56b70176245397
Haesung <koh1260@naver.com>
2025-05-07 07:01:56 +0900

    refactor: island writer에서 메모리 관련 코드 제거

--- src/domain/components/islands/island-writer.ts
+++ src/domain/components/islands/island-writer.ts
@@ -1,15 +1,12 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { IslandEntity } from 'src/domain/entities/islands/island.entity';
 import { IslandRepository } from 'src/domain/interface/island.repository';
-import { NormalIslandStorage } from 'src/domain/interface/storages/normal-island-storage';
 
 @Injectable()
 export class IslandWriter {
     constructor(
         @Inject(IslandRepository)
         private readonly islandRepository: IslandRepository,
-        @Inject(NormalIslandStorage)
-        private readonly normalIslandStorage: NormalIslandStorage,
     ) {}
 
     async create(island: IslandEntity) {
@@ -18,6 +15,5 @@ export class IslandWriter {
 
     async remove(id: string) {
         await this.islandRepository.delete(id);
-        this.normalIslandStorage.delete(id);
     }
 }
