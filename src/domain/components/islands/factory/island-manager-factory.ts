d3c5ba89a68cf04f6c60090c6b95533c386cb660
Haesung <koh1260@naver.com>
2025-05-11 07:17:44 +0900

    feat: game island service에서 담당하고 있는 기능을 다수 위임한 섬 종류별 매니저 및 팩토리 구현

---
+++ src/domain/components/islands/factory/island-manager-factory.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { DesertedIslandManager } from 'src/domain/components/islands/deserted-storage/deserted-island-manager';
+import { IslandManager } from 'src/domain/components/islands/interface/island-manager';
+import { NormalIslandManager } from 'src/domain/components/islands/normal-storage/normal-island-manager';
+import { IslandTypeEnum } from 'src/domain/types/island.types';
+
+@Injectable()
+export class IslandManagerFactory {
+    constructor(
+        private readonly normalIslandManager: NormalIslandManager,
+        private readonly desertedIslandManager: DesertedIslandManager,
+    ) {}
+
+    get(type: IslandTypeEnum): IslandManager {
+        switch (type) {
+            case IslandTypeEnum.NORMAL:
+                return this.normalIslandManager;
+            case IslandTypeEnum.DESERTED:
+                return this.desertedIslandManager;
+            default:
+                throw new Error('Invalid island type');
+        }
+    }
+}
---
+++ src/domain/components/islands/interface/island-manager.ts
@@ -0,0 +1,9 @@
+import { Player } from 'src/domain/models/game/player';
+
+export interface IslandManager {
+    canJoin(islandId: string): Promise<void>;
+    join(player: Player): Promise<void>;
+    getActiveUsers(islandId: string, myPlayerId: string): Promise<Player[]>;
+    left(islandId: string, playerId: string): Promise<void>;
+    removeEmpty(islandId: string): Promise<void>;
+}
