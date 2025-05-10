ac0c54632bda6a5dcd3b88aad75033fb8a1eaebc
Haesung <koh1260@naver.com>
2025-05-10 16:04:35 +0900

    chore: 섬 관련 메모리 구현체 주석 처리
    - 반환 타입이 Promise로 변경돼서 바꿔줘야하는데 굳이 싶어서 주석

--- src/infrastructure/storages/deserted-island-memory-storage.ts
+++ src/infrastructure/storages/deserted-island-memory-storage.ts
@@ -1,75 +1,75 @@
-import { Injectable } from '@nestjs/common';
-import { DomainExceptionType } from 'src/domain/exceptions/enum/domain-exception-type';
-import { DomainException } from 'src/domain/exceptions/exceptions';
-import { ISLAND_NOT_FOUND_MESSAGE } from 'src/domain/exceptions/message';
-import { DesertedIslandStorage } from 'src/domain/interface/storages/deserted-island-storage';
-import { LiveDesertedIsland } from 'src/domain/types/game.types';
+// import { Injectable } from '@nestjs/common';
+// import { DomainExceptionType } from 'src/domain/exceptions/enum/domain-exception-type';
+// import { DomainException } from 'src/domain/exceptions/exceptions';
+// import { ISLAND_NOT_FOUND_MESSAGE } from 'src/domain/exceptions/message';
+// import { DesertedIslandStorage } from 'src/domain/interface/storages/deserted-island-storage';
+// import { LiveDesertedIsland } from 'src/domain/types/game.types';
 
-@Injectable()
-export class DesertedIslandMemoryStorage implements DesertedIslandStorage {
-    private desertedIslands = new Map<string, LiveDesertedIsland>();
+// @Injectable()
+// export class DesertedIslandMemoryStorage implements DesertedIslandStorage {
+//     private desertedIslands = new Map<string, LiveDesertedIsland>();
 
-    createIsland(islandId: string, island: LiveDesertedIsland): void {
-        this.desertedIslands.set(islandId, island);
-    }
+//     createIsland(islandId: string, island: LiveDesertedIsland): void {
+//         this.desertedIslands.set(islandId, island);
+//     }
 
-    getIsland(islandId: string): LiveDesertedIsland {
-        const island = this.desertedIslands.get(islandId);
-        if (!island) {
-            throw new DomainException(
-                DomainExceptionType.ISLAND_NOT_FOUND,
-                1000,
-                ISLAND_NOT_FOUND_MESSAGE,
-            );
-        }
+//     getIsland(islandId: string): LiveDesertedIsland {
+//         const island = this.desertedIslands.get(islandId);
+//         if (!island) {
+//             throw new DomainException(
+//                 DomainExceptionType.ISLAND_NOT_FOUND,
+//                 1000,
+//                 ISLAND_NOT_FOUND_MESSAGE,
+//             );
+//         }
 
-        return island;
-    }
+//         return island;
+//     }
 
-    getAllIsland(): LiveDesertedIsland[] {
-        return Array.from(this.desertedIslands.values());
-    }
+//     getAllIsland(): LiveDesertedIsland[] {
+//         return Array.from(this.desertedIslands.values());
+//     }
 
-    countPlayer(islandId: string): number {
-        const island = this.desertedIslands.get(islandId);
-        if (!island) {
-            throw new DomainException(
-                DomainExceptionType.ISLAND_NOT_FOUND,
-                1000,
-                ISLAND_NOT_FOUND_MESSAGE,
-            );
-        }
+//     countPlayer(islandId: string): number {
+//         const island = this.desertedIslands.get(islandId);
+//         if (!island) {
+//             throw new DomainException(
+//                 DomainExceptionType.ISLAND_NOT_FOUND,
+//                 1000,
+//                 ISLAND_NOT_FOUND_MESSAGE,
+//             );
+//         }
 
-        return island.players.size;
-    }
+//         return island.players.size;
+//     }
 
-    addPlayerToIsland(islandId: string, playerId: string): void {
-        const island = this.desertedIslands.get(islandId);
-        if (!island) {
-            throw new DomainException(
-                DomainExceptionType.ISLAND_NOT_FOUND,
-                1000,
-                ISLAND_NOT_FOUND_MESSAGE,
-            );
-        }
+//     addPlayerToIsland(islandId: string, playerId: string): void {
+//         const island = this.desertedIslands.get(islandId);
+//         if (!island) {
+//             throw new DomainException(
+//                 DomainExceptionType.ISLAND_NOT_FOUND,
+//                 1000,
+//                 ISLAND_NOT_FOUND_MESSAGE,
+//             );
+//         }
 
-        island.players.add(playerId);
-    }
+//         island.players.add(playerId);
+//     }
 
-    getIslandStore(): Record<string, LiveDesertedIsland> {
-        return Object.fromEntries(this.desertedIslands.entries());
-    }
+//     getIslandStore(): Record<string, LiveDesertedIsland> {
+//         return Object.fromEntries(this.desertedIslands.entries());
+//     }
 
-    getPlayerIdsByIslandId(islandId: string): string[] {
-        const island = this.desertedIslands.get(islandId);
-        if (!island) {
-            throw new DomainException(
-                DomainExceptionType.ISLAND_NOT_FOUND,
-                1000,
-                ISLAND_NOT_FOUND_MESSAGE,
-            );
-        }
+//     getPlayerIdsByIslandId(islandId: string): string[] {
+//         const island = this.desertedIslands.get(islandId);
+//         if (!island) {
+//             throw new DomainException(
+//                 DomainExceptionType.ISLAND_NOT_FOUND,
+//                 1000,
+//                 ISLAND_NOT_FOUND_MESSAGE,
+//             );
+//         }
 
-        return Array.from(island.players);
-    }
-}
+//         return Array.from(island.players);
+//     }
+// }
--- src/infrastructure/storages/normal-island-memory-storage.ts
+++ src/infrastructure/storages/normal-island-memory-storage.ts
@@ -1,78 +1,78 @@
-import { Injectable } from '@nestjs/common';
-import { DomainExceptionType } from 'src/domain/exceptions/enum/domain-exception-type';
-import { DomainException } from 'src/domain/exceptions/exceptions';
-import { ISLAND_NOT_FOUND_MESSAGE } from 'src/domain/exceptions/message';
-import { NormalIslandStorage } from 'src/domain/interface/storages/normal-island-storage';
-import { LiveNormalIsland } from 'src/domain/types/game.types';
+// import { Injectable } from '@nestjs/common';
+// import { DomainExceptionType } from 'src/domain/exceptions/enum/domain-exception-type';
+// import { DomainException } from 'src/domain/exceptions/exceptions';
+// import { ISLAND_NOT_FOUND_MESSAGE } from 'src/domain/exceptions/message';
+// import { NormalIslandStorage } from 'src/domain/interface/storages/normal-island-storage';
+// import { LiveNormalIsland } from 'src/domain/types/game.types';
 
-@Injectable()
-export class NormalIslandMemoryStorage implements NormalIslandStorage {
-    private normalIslands = new Map<string, LiveNormalIsland>();
+// @Injectable()
+// export class NormalIslandMemoryStorage implements NormalIslandStorage {
+//     private normalIslands = new Map<string, LiveNormalIsland>();
 
-    createIsland(islandId: string, island: LiveNormalIsland): void {
-        this.normalIslands.set(islandId, island);
-    }
+//     createIsland(islandId: string, island: LiveNormalIsland): void {
+//         this.normalIslands.set(islandId, island);
+//     }
 
-    getIsland(islandId: string): LiveNormalIsland {
-        const island = this.normalIslands.get(islandId) ?? null;
-        if (!island)
-            throw new DomainException(
-                DomainExceptionType.ISLAND_NOT_FOUND,
-                1000,
-                ISLAND_NOT_FOUND_MESSAGE,
-            );
+//     getIsland(islandId: string): LiveNormalIsland {
+//         const island = this.normalIslands.get(islandId) ?? null;
+//         if (!island)
+//             throw new DomainException(
+//                 DomainExceptionType.ISLAND_NOT_FOUND,
+//                 1000,
+//                 ISLAND_NOT_FOUND_MESSAGE,
+//             );
 
-        return island;
-    }
+//         return island;
+//     }
 
-    getAllIsland(): LiveNormalIsland[] {
-        return Array.from(this.normalIslands.values());
-    }
+//     getAllIsland(): LiveNormalIsland[] {
+//         return Array.from(this.normalIslands.values());
+//     }
 
-    countPlayer(islandId: string): number {
-        const island = this.normalIslands.get(islandId);
-        if (!island) {
-            throw new DomainException(
-                DomainExceptionType.ISLAND_NOT_FOUND,
-                1000,
-                ISLAND_NOT_FOUND_MESSAGE,
-            );
-        }
+//     countPlayer(islandId: string): number {
+//         const island = this.normalIslands.get(islandId);
+//         if (!island) {
+//             throw new DomainException(
+//                 DomainExceptionType.ISLAND_NOT_FOUND,
+//                 1000,
+//                 ISLAND_NOT_FOUND_MESSAGE,
+//             );
+//         }
 
-        return island.players.size;
-    }
+//         return island.players.size;
+//     }
 
-    addPlayerToIsland(islandId: string, playerId: string): void {
-        const island = this.normalIslands.get(islandId);
-        if (!island) {
-            throw new DomainException(
-                DomainExceptionType.ISLAND_NOT_FOUND,
-                1000,
-                ISLAND_NOT_FOUND_MESSAGE,
-            );
-        }
+//     addPlayerToIsland(islandId: string, playerId: string): void {
+//         const island = this.normalIslands.get(islandId);
+//         if (!island) {
+//             throw new DomainException(
+//                 DomainExceptionType.ISLAND_NOT_FOUND,
+//                 1000,
+//                 ISLAND_NOT_FOUND_MESSAGE,
+//             );
+//         }
 
-        island.players.add(playerId);
-    }
+//         island.players.add(playerId);
+//     }
 
-    getIslandStore(): Record<string, LiveNormalIsland> {
-        return Object.fromEntries(this.normalIslands.entries());
-    }
+//     getIslandStore(): Record<string, LiveNormalIsland> {
+//         return Object.fromEntries(this.normalIslands.entries());
+//     }
 
-    getPlayerIdsByIslandId(islandId: string): string[] {
-        const island = this.normalIslands.get(islandId);
-        if (!island) {
-            throw new DomainException(
-                DomainExceptionType.ISLAND_NOT_FOUND,
-                1000,
-                ISLAND_NOT_FOUND_MESSAGE,
-            );
-        }
+//     getPlayerIdsByIslandId(islandId: string): string[] {
+//         const island = this.normalIslands.get(islandId);
+//         if (!island) {
+//             throw new DomainException(
+//                 DomainExceptionType.ISLAND_NOT_FOUND,
+//                 1000,
+//                 ISLAND_NOT_FOUND_MESSAGE,
+//             );
+//         }
 
-        return Array.from(island.players);
-    }
+//         return Array.from(island.players);
+//     }
 
-    delete(islandId: string): void {
-        this.normalIslands.delete(islandId);
-    }
-}
+//     delete(islandId: string): void {
+//         this.normalIslands.delete(islandId);
+//     }
+// }
