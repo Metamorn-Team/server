5e477979e0521fa005cd3bc0c4924f10c550d690
Haesung <koh1260@naver.com>
2025-05-15 03:54:30 +0900

    mmorntype@1.0.60

--- src/presentation/dto/game/response/active-players.response.ts
+++ src/presentation/dto/game/response/active-players.response.ts
@@ -3,6 +3,7 @@ export interface ActivePlayer {
     readonly nickname: string;
     readonly tag: string;
     readonly avatarKey: string;
+    readonly lastActivity: number;
     readonly x: number;
     readonly y: number;
 }
