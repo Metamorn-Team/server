e7ba59df47bc6cede8e550ef61ec249f6d585192
Haesung <koh1260@naver.com>
2025-05-19 19:07:15 +0900

    test: 섬 하트비트 단위 테스트 작성
    - 정상 동작

--- test/unit/services/game.service.unit-spec.ts
+++ test/unit/services/game.service.unit-spec.ts
@@ -123,14 +123,14 @@ describe('GameService', () => {
         });
 
         it('공격 범위에 속하는 모든 플레이어는 공격 대상이 된다', async () => {
-            // 공격 박스 내의 가장 좌측 좌표
+            // 공격 범위 내의 가장 좌측 좌표
             const attackedLeft = generatePlayerModel({
                 roomId: island.id,
                 islandType: IslandTypeEnum.DESERTED,
                 x: PLAYER_HIT_BOX.PAWN.RADIUS + 1,
                 y: 0,
             });
-            // 공격 박스 내의 가장 우측 좌표
+            // 공격 범위 내의 가장 우측 좌표
             const attackedRight = generatePlayerModel({
                 roomId: island.id,
                 islandType: IslandTypeEnum.DESERTED,
@@ -159,15 +159,15 @@ describe('GameService', () => {
             ).toEqual([attackedLeft.id, attackedRight.id].sort());
         });
 
-        it('공격 박스 경계에 겹치는 플레이어는 공격을 받지 않는다', async () => {
-            // 공격 박스 좌측 경계에 걸침
+        it('공격 범위 경계에 겹치는 플레이어는 공격을 받지 않는다', async () => {
+            // 공격 범위 좌측 경계에 걸침
             const attackedPlayer1 = generatePlayerModel({
                 roomId: island.id,
                 islandType: IslandTypeEnum.DESERTED,
                 x: -PLAYER_HIT_BOX.PAWN.RADIUS,
                 y: 0,
             });
-            // 공격 박스 우측 경계에 걸침
+            // 공격 범위 우측 경계에 걸침
             const attackedPlayer2 = generatePlayerModel({
                 roomId: island.id,
                 islandType: IslandTypeEnum.DESERTED,
@@ -191,6 +191,56 @@ describe('GameService', () => {
 
             expect(result.attacker.id).toEqual(attacker.id);
             expect(result.attackedPlayers.length).toEqual(0);
+        });
+    });
+
+    describe('플레이어 하트비트', () => {
+        let island: LiveDesertedIsland;
+        let player: Player;
+
+        beforeEach(async () => {
+            island = generateDesertedIslandModel();
+            player = generatePlayerModel({
+                isFacingRight: true,
+                roomId: island.id,
+                islandType: IslandTypeEnum.DESERTED,
+                x: 0,
+                y: 0,
+            });
+
+            await desertedIslandStorage.createIsland(island);
+            playerMemoryStorage.addPlayer(player);
+            await desertedIslandStorage.addPlayerToIsland(island.id, player.id);
+        });
+
+        it('자신을 포함한 같은 섬에 있는 플레이어의 마지막 활동 시간을 조회한다', async () => {
+            const now = Date.now();
+            const otherPlayers = Array.from({ length: 3 }, (_, i) =>
+                generatePlayerModel({
+                    tag: `tag${i}`,
+                    roomId: island.id,
+                    lastActivity: now - i,
+                }),
+            );
+            for (const player of otherPlayers) {
+                playerMemoryStorage.addPlayer(player);
+                await desertedIslandStorage.addPlayerToIsland(
+                    island.id,
+                    player.id,
+                );
+            }
+
+            const heartbeats = await gameService.hearbeatFromIsland(player.id);
+            const sortedHeartbeats = heartbeats.sort(
+                (a, b) => b.lastActivity - a.lastActivity,
+            );
+            const expected = [...otherPlayers, player].map((player) => ({
+                id: player.id,
+                lastActivity: player.lastActivity,
+            }));
+
+            expect(heartbeats.length).toEqual(otherPlayers.length + 1);
+            expect(sortedHeartbeats).toEqual(expected);
         });
     });
 });
