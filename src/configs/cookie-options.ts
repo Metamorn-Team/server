0e9d0d1a929ca7109b812f0708fdf177fb147d86
Haesung <koh1260@naver.com>
2025-05-20 17:49:38 +0900

    chore: 쿠키 samesite 변경

--- src/configs/cookie-options.ts
+++ src/configs/cookie-options.ts
@@ -3,7 +3,7 @@ import { CookieOptions } from 'express';
 export const cookieOptions = (): CookieOptions => ({
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: 'none',
     domain: process.env.NODE_ENV === 'production' ? 'livisland.com' : undefined,
     maxAge: Number(process.env.REFRESH_COOKIE_TIME),
 });
