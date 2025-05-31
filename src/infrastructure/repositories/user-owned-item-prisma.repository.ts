78236ec5aa4544c67d01b1c0e58658f9ae36a762
Haesung <koh1260@naver.com>
2025-06-01 03:53:50 +0900

    feat: 소유 중인 아이템 조회 repo 메서드 구현

--- src/infrastructure/repositories/user-owned-item-prisma.repository.ts
+++ src/infrastructure/repositories/user-owned-item-prisma.repository.ts
@@ -3,12 +3,61 @@ import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-pr
 import { Injectable } from '@nestjs/common';
 import { UserOwnedItemEntity } from 'src/domain/entities/user-owned-items/user-owned-item.entity';
 import { UserOwnedItemRepository } from 'src/domain/interface/user-owned-item.repository';
+import {
+    ItemTypeEnum,
+    ItemGradeEnum,
+    Item,
+    convertNumberToItemGrade,
+    convertNumberToItemType,
+} from 'src/domain/types/item.types';
 
 @Injectable()
 export class UserOwnedItemPrismaRepository implements UserOwnedItemRepository {
     constructor(
         private readonly txHost: TransactionHost<TransactionalAdapterPrisma>,
     ) {}
+
+    async findAllByTypeAndGrade(
+        userId: string,
+        itemType: ItemTypeEnum,
+        grade: ItemGradeEnum,
+    ): Promise<Item[]> {
+        const result = await this.txHost.tx.userOwnedItem.findMany({
+            select: {
+                item: {
+                    select: {
+                        id: true,
+                        name: true,
+                        description: true,
+                        itemType: true,
+                        key: true,
+                        grade: true,
+                    },
+                },
+            },
+            where: {
+                userId,
+                item: {
+                    grade,
+                    itemType,
+                },
+            },
+            orderBy: {
+                item: {
+                    name: 'asc',
+                },
+            },
+        });
+
+        return result.map((item) => {
+            const { itemType, grade, ...itemData } = item.item;
+            return {
+                ...itemData,
+                type: convertNumberToItemType(itemType),
+                grade: convertNumberToItemGrade(grade),
+            };
+        });
+    }
 
     async save(data: UserOwnedItemEntity): Promise<void> {
         await this.txHost.tx.userOwnedItem.create({ data });
