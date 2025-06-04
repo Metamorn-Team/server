b9334af2d5439a70f6464b62ee79da8d2966f2f0
Haesung <koh1260@naver.com>
2025-06-04 20:41:42 +0900

    chore: prisma 로깅 제거

--- src/infrastructure/prisma/prisma.service.ts
+++ src/infrastructure/prisma/prisma.service.ts
@@ -3,11 +3,11 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    constructor() {
-        super({
-            log: ['info', 'warn', 'error'],
-        });
-    }
+    // constructor() {
+    //     super({
+    //         log: ['info', 'warn', 'error'],
+    //     });
+    // }
 
     async onModuleInit() {
         await this.$connect();
