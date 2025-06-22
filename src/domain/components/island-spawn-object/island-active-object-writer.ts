646503e57997c7605f1ec857495f5c9e83d3a3d8
Haesung <koh1260@naver.com>
2025-06-22 21:01:02 +0900

    feat: 스폰 오브젝트 저장 기능 구현 (memory)
    - 실제 공격 등의 연산은 memory 레벨에서 진행하기 위함

---
+++ src/domain/components/island-spawn-object/island-active-object-writer.ts
@@ -0,0 +1,15 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ActiveObject } from 'src/domain/types/spawn-object/active-object';
+import { IslandActiveObjectStorage } from 'src/infrastructure/memories/island-active-object-storage';
+
+@Injectable()
+export class IslandActiveObjectWriter {
+    constructor(
+        @Inject(IslandActiveObjectStorage)
+        private readonly islandSpawnObjectMemoryStorage: IslandActiveObjectStorage,
+    ) {}
+
+    createMany(objects: ActiveObject[]) {
+        this.islandSpawnObjectMemoryStorage.createMany(objects);
+    }
+}
