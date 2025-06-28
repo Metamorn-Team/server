4997b48191b17be8d6f23cc614aa2b92d02e27aa
Haesung <koh1260@naver.com>
2025-06-28 18:06:11 +0900

    feat: 오브젝트 조회 케이스 추가

--- src/domain/components/island-spawn-object/island-active-object-reader.ts
+++ src/domain/components/island-spawn-object/island-active-object-reader.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { DomainExceptionType } from 'src/domain/exceptions/enum/domain-exception-type';
+import { DomainException } from 'src/domain/exceptions/exceptions';
+import { OBJECT_NOT_FOUND_MESSAGE } from 'src/domain/exceptions/message';
 import { ActiveObject } from 'src/domain/types/spawn-object/active-object';
 import { IslandActiveObjectStorage } from 'src/infrastructure/memories/island-active-object-storage';
 
@@ -14,5 +17,28 @@ export class IslandActiveObjectReader {
 
     readByIds(islandId: string, ids: string[]): ActiveObject[] {
         return this.islandSpawnObjectMemoryStorage.readByIds(islandId, ids);
+    }
+
+    readOne(islandId: string, objectId: string): ActiveObject {
+        const object = this.islandSpawnObjectMemoryStorage.readOne(
+            islandId,
+            objectId,
+        );
+        if (!object) {
+            throw new DomainException(
+                DomainExceptionType.OBJECT_NOT_FOUND,
+                OBJECT_NOT_FOUND_MESSAGE(islandId),
+            );
+        }
+
+        return object;
+    }
+
+    readAlive(islandId: string): ActiveObject[] {
+        return this.islandSpawnObjectMemoryStorage.readAlive(islandId);
+    }
+
+    readDead(islandId: string): ActiveObject[] {
+        return this.islandSpawnObjectMemoryStorage.readDead(islandId);
     }
 }
