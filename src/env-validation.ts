2ba1202668b4ebe189152f9a6979c2e7c535facf
Haesung <koh1260@naver.com>
2025-06-30 09:13:20 +0900

    feat: production 환경에서 cloudwatch에 로그 전송

--- src/env-validation.ts
+++ src/env-validation.ts
@@ -20,4 +20,11 @@ export const validationSchema = Joi.object({
     AWS_ACCESS_KEY_ID: Joi.string().required(),
     AWS_SECRET_ACCESS_KEY: Joi.string().required(),
     AWS_BUCKEY_NAME: Joi.string().required(),
+
+    // AWS Cloudwatch
+    CLOUDWATCH_LOG_GROUP: Joi.string().required(),
+    CLOUDWATCH_LOG_STREAM: Joi.string().required(),
+    CLOUDWATCH_REGION: Joi.string().required(),
+    CLOUDWATCH_KEY_ID: Joi.string().required(),
+    CLOUDWATCH_SECRET_KEY: Joi.string().required(),
 });
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from 'src/app.module';
 import { setupSwagger } from 'src/configs/swagger/setup-swagger';
+import { winstonLogger } from 'src/configs/winston/winston-options';
 import { MessagePackIoAdapter } from 'src/presentation/gateway/adapter/msg-pack-adapter';
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+        logger: winstonLogger,
+    });
 
     const wsAdapter = new MessagePackIoAdapter(app);
 
@@ -15,6 +18,6 @@ async function bootstrap() {
     app.useWebSocketAdapter(wsAdapter);
     setupSwagger(app);
 
-    await app.listen(3000);
+    await app.listen(4000);
 }
 bootstrap();
