ce973aa90e4a58b8fc53439228d3de39efe33c01
Haesung <koh1260@naver.com>
2025-08-06 22:05:01 +0900

    feat: private island manager 구현체

---
+++ src/modules/islands/private-island-manager.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { PrivateIslandManager } from 'src/domain/components/islands/private-storage/private-island-manager';
+import { IslandComponentModule } from 'src/modules/islands/island-component.module';
+import { IslandJoinComponentModule } from 'src/modules/island-joins/island-join-component.module';
+import { IslandActiveObjectComponentModule } from 'src/modules/island-spawn-objects/island-active-object-component.module';
+import { RespawnQueueManagerModule } from 'src/modules/island-spawn-objects/respawn-queue-manager.module';
+import { EquipmentComponentModule } from 'src/modules/equipments/equipment-component.module';
+import { PlayerStorageComponentModule } from 'src/modules/users/player-storage-component.module';
+import { RedisTransactionManagerModule } from 'src/infrastructure/redis/redis-transaction-manger.module';
+import { LivePrivateIslandComponentModule } from 'src/modules/islands/live-private-island-component.module';
+
+@Module({
+    imports: [
+        LivePrivateIslandComponentModule,
+        PlayerStorageComponentModule,
+        IslandJoinComponentModule,
+        IslandComponentModule,
+        EquipmentComponentModule,
+        IslandActiveObjectComponentModule,
+        RedisTransactionManagerModule,
+        RespawnQueueManagerModule,
+    ],
+    providers: [PrivateIslandManager],
+    exports: [PrivateIslandManager],
+})
+export class PrivateIslandManagerModule {}
